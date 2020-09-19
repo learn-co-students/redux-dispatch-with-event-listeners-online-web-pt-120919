fb0aae534680b55f94c02354fe4be26d0f39b231
Sehrish88 <sehrishbalouch4@gmail.com>
2020-09-19 13:29:13 -0400

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,1 +1,11 @@
 // add code snippets from README
+let state = {count: 0}
+ 
+function reducer(state, action){
+  switch (action.type) {
+    case 'INCREASE_COUNT':
+      return {count: state.count + 1};
+    default:
+      return state;
+  }
+}
